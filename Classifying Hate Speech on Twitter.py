8d1b07e6ca24c967d6c801ade97c52e31316213a
nidhi988 <57409772+nidhi988@users.noreply.github.com>
2020-08-13 23:52:28 +0530

    Add files via upload

---
+++ Classifying Hate Speech on Twitter.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+# load training data
+train = pd.read_csv('BERT_proj/train_E6oV3lV.csv', encoding='iso-8859-1')
+train.shape
+
+
+# In[ ]:
+
+
+import re
+
+# clean text from noise
+def clean_text(text):
+    # filter to allow only alphabets
+    text = re.sub(r'[^a-zA-Z\']', ' ', text)
+    
+    # remove Unicode characters
+    text = re.sub(r'[^\x00-\x7F]+', '', text)
+    
+    # convert to lowercase to maintain consistency
+    text = text.lower()
+       
+    return text
+
+train['clean_text'] = train.tweet.apply(clean_text)
+
+
+# In[ ]:
+
+
+from sklearn.model_selection import train_test_split
+
+# split into training and validation sets
+X_tr, X_val, y_tr, y_val = train_test_split(train.clean_text, train.label, test_size=0.25, random_state=42)
+
+print('X_tr shape:',X_tr.shape)
+
+
+# In[ ]:
+
+
+from bert_serving.client import BertClient
+
+# make a connection with the BERT server using it's ip address
+bc = BertClient(ip="YOUR_SERVER_IP")
+# get the embedding for train and val sets
+X_tr_bert = bc.encode(X_tr.tolist())
+X_val_bert = bc.encode(X_val.tolist())
+
+
+# In[ ]:
+
+
+from sklearn.linear_model import LogisticRegression
+
+# LR model
+model_bert = LogisticRegression()
+# train
+model_bert = model_bert.fit(X_tr_bert, y_tr)
+# predict
+pred_bert = model_bert.predict(X_val_bert)
+
+
+# In[ ]:
+
+
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(y_val, pred_bert))
+
+
+# In[ ]:
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+Classifying Hate Speech on Twitter
+
+We will use BERT to extract embeddings from each tweet in the dataset and then use these embeddings to train a text classification model.
